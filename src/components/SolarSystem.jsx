49bbfe6ece2b15b684613e858917931caca9c6ad
Kaio G <kaiogreco91@gmail.com>
2022-02-18 20:17:05 -0300

    req.6 - SolarSystem Component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div>
-        <div data-testid="solar-system" />
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          {planets.map((element) => (
+            <PlanetCard
+              key={ element.name }
+              planetName={ element.name }
+              planetImage={ element.image }
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
-
 export default SolarSystem;
