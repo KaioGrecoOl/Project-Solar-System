fd31acbd14106475776c41dcadb0f49b425ba840
Kaio G <kaiogreco91@gmail.com>
2022-02-18 16:02:32 -0300

    req.2 - SolarSystem Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        
+      </div>
+
+    );
+  }
+}
+
+export default SolarSystem;
