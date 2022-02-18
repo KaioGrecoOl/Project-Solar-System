98a30a768d5f64dd8dc622fe513b2ea66e8e20fb
Kaio G <kaiogreco91@gmail.com>
2022-02-18 15:31:54 -0300

    req.1 - Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
