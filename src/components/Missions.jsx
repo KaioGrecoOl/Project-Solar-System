13d6671d61311720c7d4177aa40896c9bdc189d0
Kaio G <kaiogreco91@gmail.com>
2022-02-19 21:26:07 -0300

    req.10 - Missions Render Component

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missions.map((element) => (
+            <MissionCard
+              key={ element.name }
+              name={ element.name }
+              year={ element.year }
+              country={ element.country }
+              destination={ element.destination }
+            />
+          ))}
+        </div>
       </div>
     );
   }
